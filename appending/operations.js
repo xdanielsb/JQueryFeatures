a8f18921d2b2d893145fc188eea5c2bebfca8bda
xdanielsb <dfsantosbu@unal.edu.co>
2017-03-26 23:43:38 -0500

    Add algorithms for appending.

---
+++ appending/operations.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  //change all li to the text orlando
+  var new_data = $("<p> The teacher diosa is a good teacher </p>")
+  $("#data").append(new_data)
+  $("#button").remove()
+});
