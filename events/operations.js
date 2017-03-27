58c0612c3e74428a216a96966b52823f63d0db06
xdanielsb <dfsantosbu@unal.edu.co>
2017-03-26 23:44:26 -0500

    Add algorithms for handling events.

---
+++ events/operations.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  //Listen an event
+  $("#accept").on("click", function(){
+        alert("hello")
+  });
+
+  $(".same").on("click", function(){
+      //Convert this to a jquery object
+      $(this).remove();
+  });
+
+  $(".add").on("click", function(){
+      //Convert this to a jquery object
+      $(this).append($("<button> hello </button>"))
+  });
+
+  //Only target buttos that are inside a vacation class
+  $(".vacation").on("click","button", function(){
+      alert("hello");
+  });
+
+});
